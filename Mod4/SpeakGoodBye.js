bee0444ed78ef824a74e8a9fff8e6a3227ed8d07
samandsom <samanyazdani1371@gmail.com>
2016-11-12 19:46:57 +0330

    New Assignment1

--- Mod4/SpeakGoodBye.js
+++ Mod4/SpeakGoodBye.js
@@ -13,13 +13,13 @@
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 (function (window){
 	var byeSpeaker = {};
-	byeSpeaker.speakWord = "Good Bye";
+	var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 	byeSpeaker.speak = function (name) {
-	  console.log(byeSpeaker.speakWord + " " + name);
+	  console.log(speakWord + " " + name);
 
 }	
 	window.byeSpeaker = byeSpeaker;
--- Mod4/SpeakHello.js
+++ Mod4/SpeakHello.js
@@ -10,12 +10,12 @@
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 (function (window) {
 	var helloSpeaker = {};
-	helloSpeaker.speakWord = "Hello";
+	var speakWord = "Hello";
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 helloSpeaker.speak = function (name) {
-  console.log(helloSpeaker.speakWord + " " + name);
+  console.log(speakWord + " " + name);
 }
 	window.helloSpeaker = helloSpeaker;
 })(window);
